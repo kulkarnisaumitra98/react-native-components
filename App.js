cb495f719e18e1eed9c10f1ea720769bf0b44785
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2020-02-12 19:28:59 +0530

    add titledInput

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TitledInput from './src/components/TitledInput/TitledInput';
 
 export default function App() {
+  const [value, setvalue] = useState('')
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputCont}>
+        <TitledInput
+          title="EMAIL ID/ PHONE NUMBER"
+          config={{
+            value: value,
+            placeholder: "Email",
+            secureText: false,
+            onChangeText: (ipvalue) => setvalue(ipvalue),
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -14,6 +27,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingTop: 24,
   },
+
+  inputCont: {
+    width: '90%',
+    // borderWidth: 1,
+  }
 });
---
+++ src/components/TitledInput/TitledInput.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Text, View, StyleSheet, TextInput} from 'react-native';
+
+const TitledInput = ({
+    containerStyle,
+    titleStyle,
+    textInputStyle,    
+    config,
+    title,
+}) => {
+    return (
+        <View style={[styles.container, containerStyle]}>
+            {title ? <Text style={[styles.title, titleStyle]}>{title}</Text> : <React.Fragment/>}                                         
+            <TextInput
+                style={[styles.inputContainer, textInputStyle]}
+                {...config}
+            />                          
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        marginTop: 16,
+        position: 'relative',
+    },
+
+    inputContainer:{
+        borderRadius: 4,
+        width: '100%',
+        marginTop: 8,
+        borderBottomWidth: 0.5,
+        elevation: 2,
+        backgroundColor: '#fff',
+        borderColor: '#ddd',
+        letterSpacing: 0.3,
+        textAlignVertical: 'center',
+        padding: 4,
+        fontSize: 12,
+        
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+    },
+
+    title: {
+        fontSize: 12,
+        fontFamily: 'sans-serif',
+        color: '#2b2d38'
+    },
+});
+
+export default TitledInput;
